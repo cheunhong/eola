cee624a4c95ad317e2acb0da3b95f414b64a5464
cheunhong <chlin6755@gmail.com>
2024-01-04 04:28:06 +0800

    update lambda handler

--- handlers/lambda.py
+++ handlers/lambda.py
@@ -1,41 +1,20 @@
-import json
-from datetime import datetime
-
 import duckdb
+from pydantic import BaseModel
 
 
-def serialize_datetime(obj):
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise TypeError(f" Object of type {type(obj)} is not JSON serializable")
+class Event(BaseModel):
+    queries: list[str]
+    session: str
 
 
 def handler(event, context):
-    query = event.get("query")
-    if not query:
-        return {"statusCode": 401, "body": "Invalid request body"}
-
     try:
-        with duckdb.connect() as conn:
-            conn.load_extension("httpfs")
-            # DuckDB auto-loaded env vars
-            conn.execute(
-                """
-                RESET s3_region;
-                RESET s3_access_key_id;
-                RESET s3_secret_access_key;
-                RESET s3_session_token;
-                """
-            )
-            result = conn.execute(query)
-            body = {
-                "columns": result.description,
-                "rows": json.loads(
-                    json.dumps(result.fetchall(), default=serialize_datetime)
-                ),
-            }
+        event = Event(**event)
+        for query in event.queries:
+            with duckdb.connect(f"/tmp/{event.session}.db") as conn:
+                conn.execute(query)
 
-        return {"statusCode": 200, "body": body}
+        return {"statusCode": 200}
 
     except Exception as e:
         return {"statusCode": 400, "body": str(e)}
